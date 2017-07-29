6a152b50fee1d4a9201fb93c851ffa627524d881
bigfang <bitair@gmail.com>
2017-07-29 19:50:11 +0800

    1.3更新了读取环境变量的方式

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,6 @@ use Mix.Config
 # which you typically run after static files are built.
 config :hotchpotch, HotchpotchWeb.Endpoint,
   load_from_system_env: true,
-  http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
