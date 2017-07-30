d17ff76c3f836581fb5fb1e0accbb624b9dfb7cb
bigfang <bitair@gmail.com>
2017-07-30 08:53:46 +0800

    升级priv/gettext与1.3保持一致

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,7 +5,7 @@
 # Inside the script, you can read and write to any of your
 # repositories directly:
 #
-#     Hotchpotch.Repo.insert!(%Hotchpotch.SomeModel{})
+#     Hotchpotch.Repo.insert!(%Hotchpotch.SomeSchema{})
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
