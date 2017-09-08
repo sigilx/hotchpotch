a27d930ece9a8b4f4b042e79186c9244ec9315cb
bigfang <bitair@gmail.com>
2017-09-08 11:07:32 +0800

    鼠标偏移了几个像素

--- assets/js/board/components/h-board.vue
+++ assets/js/board/components/h-board.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="d-flex justify-content-center">
-    <div>
     <canvas id="canvas" class="whiteboard" width="800" height="500"
       @mousedown="onMouseDown"
       @mouseup="onMouseUp"
       @mousemove="onMouseMove">
     </canvas>
-    </div>
   </div>
 </template>
 
 <style scoped>
 .whiteboard {
   border: 1px solid #ddd;
-  width: 100%;
+  width: auto;
   height: auto;
   background-color:whitesmoke;
 }
@@ -31,7 +29,7 @@ export default {
   },
   methods: {
     drawLine: function (x0, y0, x1, y1) {
-      if (this.drawing ) {
+      if (this.drawing) {
         this.ctx.beginPath();
         this.ctx.moveTo(x0, y0);
         this.ctx.lineTo(x1, y1);
@@ -47,7 +45,6 @@ export default {
       this.currY = e.offsetY
     },
     onMouseUp: function (e) {
-      if (!this.drawing) { return; }
       this.drawing = false;
       this.drawLine(this.currX, this.currY, e.offsetX, e.offsetY)
     },
